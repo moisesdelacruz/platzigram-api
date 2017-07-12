e616bd2aac3502f231ffbb314a589fd5af7105a9
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2017-07-12 16:58:45 -0400

    Add endpoint GET / POST picure.
    * creation of endpoint get and post (TDD).
    * add stub module as module Db of proof.
    * add fixtures module with method getImage as object of proof.

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+export default {
+  getImage () {
+    return {
+      id: 'e8fa88c0-5b6f-4842-bb15-2febb834345c',
+      publicId: '2PkD0R5Y2AI4tNFvvm1WdG',
+      userId: 'platzigram',
+      src: 'http://platzigram.test/2PkD0R5Y2AI4tNFvvm1WdG.jpg',
+      description: '#awesome',
+      tags: [ 'awesome' ],
+      createdAt: new Date().toString()
+    }
+  }
+}
