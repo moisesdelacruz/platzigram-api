f1d00e78a7a240a872e127bcaef313a1b4550bf1
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2017-07-12 20:30:04 -0400

    Valid Token.
    * add jwt for POST pictures (TDD).
    * add field: secret to file config.js.

--- test/pictures-test.js
+++ test/pictures-test.js
@@ -6,6 +6,8 @@ import listen from 'test-listen'
 import request from 'request-promise'
 import fixtures from './fixtures'
 import pictures from '../pictures'
+import utils from '../lib/utils'
+import config from '../config'
 
 test.beforeEach(async t => {
   let srv = micro(pictures)
@@ -21,7 +23,7 @@ test('GET /:id', async t => {
   t.deepEqual(body, image)
 })
 
-test('POST /', async t => {
+test('no token POST /', async t => {
   let image = fixtures.getImage()
   let url = t.context.url
 
@@ -32,7 +34,30 @@ test('POST /', async t => {
     body: {
       description: image.description,
       src: image.src,
-      userId: image.src.userId
+      userId: image.userId
+    },
+    resolveWithFullResponse: true
+  }
+
+  await t.throws(request(options), /invalid token/)
+})
+
+test('secure POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+  let token = await utils.signToken({ userId: image.userId }, config.secret)
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
     },
     resolveWithFullResponse: true
   }
@@ -41,6 +66,29 @@ test('POST /', async t => {
 
   t.is(response.statusCode, 201)
   t.deepEqual(response.body, image)
+})
+
+test('invalid token POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+  let token = await utils.signToken({ userId: 'hacky' }, config.secret)
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    resolveWithFullResponse: true
+  }
+
+  await t.throws(request(options), /invalid token/)
 })
 
 test('POST /:id/like', async t => {
